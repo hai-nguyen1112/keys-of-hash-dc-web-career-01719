373be931443560fa19fb46ea7fe9bb4e2ac800dd
hai-nguyen1112 <hainguyen871112@gmail.com>
2018-12-27 15:30:59 -0500

    My keys of hash commit

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    keys_array = []
+    self.each do |key, value|
+      arguments.each do |argument|
+        if argument == value
+          keys_array << key
+        end
+      end
+    end
+    keys_array
   end
 end
